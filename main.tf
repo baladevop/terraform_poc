9e197165f292cb93e1c62a0a5a2f4753376a8135
baladevop <88687299+baladevop@users.noreply.github.com>
2022-01-05 22:26:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ resource "aws_s3_bucket" "demo-mcd058" {
 
 resource "aws_instance" "MCDpoc"{
     count = "${var.env == "false" ? 1: 0}"
-    ami = "ami-0b9064170e32bde34"
+    ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro" 
 }
